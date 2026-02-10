f5e91a7e20884548c4aa6cda404824aff8c8ad22
wibus-wee <62133302+wibus-wee@users.noreply.github.com>
2026-02-10 11:05:51 +0800

    feat: add script tag for analytics in layout component

--- www/app/layout.tsx
+++ www/app/layout.tsx
@@ -10,6 +10,9 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <script defer src="https://u.wibus.ren/script.js" data-website-id="24b5b50a-ddb7-4354-b270-e3e4274a8bdc" />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
